e9f2b709063b3e1a8e83521ac41cb74f5257d6cd
Vinman <vinman.cub@gmail.com>
2023-08-31 17:43:04 +0800

    [feat] add real lite gripper control example

---
+++ xarm_planner/launch/test_lite_gripper_realmove.launch.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2021, UFACTORY, Inc.
+# All rights reserved.
+#
+# Author: Vinman <vinman.wen@ufactory.cc> <vinman.cub@gmail.com>
+
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import OpaqueFunction
+
+def launch_setup(context, *args, **kwargs):
+    prefix = LaunchConfiguration('prefix', default='')
+    hw_ns = LaunchConfiguration('hw_ns', default='ufactory')
+    lite_gripper_node_test = Node(
+        name='test_lite_gripper_realmove',
+        package='xarm_planner',
+        executable='test_lite_gripper_realmove',
+        output='screen',
+        parameters=[
+            {
+                'robot_type': prefix,
+                'hw_ns': hw_ns
+            },
+        ],
+    )
+    return [
+        lite_gripper_node_test
+    ]
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        OpaqueFunction(function=launch_setup)
+    ])
+    
