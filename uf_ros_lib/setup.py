33d894fcad408f5434082c0f851804f5b3f33b1d
Vinman <vinman.cub@gmail.com>
2024-04-18 10:40:08 +0800

    [fix] fix `uf_ros_lib only compiles with --symlink-install option`

--- uf_ros_lib/setup.py
+++ uf_ros_lib/setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 package_name = 'uf_ros_lib'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
